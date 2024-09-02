bd651fccde4385c6f6cb3a73b52bd3e8ef0eb3b0
Hong Than Brathna <hangbrathna10@gmail.com>
2024-09-02 15:54:18 +0700

    Data Fetching using hook

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./services/store";
 import './index.css';
 
-const container = document.getElementById('root')!;
-const root = createRoot(container);
+// Safely handle the case where `el` might be null
+const el = document.getElementById('root');
+if (el) {
+  const root = ReactDOM.createRoot(el);
 
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App /> 
+      </Provider>
+    </React.StrictMode>
+  );
+} else {
+  console.error('Failed to find the root element');
+}
